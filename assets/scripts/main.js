946d88442f6bffc894b717a0b6ac96050d7b9f93
Eric Nguyen <ern005@ucsd.edu>
2025-05-27 14:43:26 -0700

    Finished Expose section

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -68,6 +68,12 @@ async function getRecipes() {
   // EXPOSE - START (All expose numbers start with A)
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
+
+  const storage = localStorage.getItem('recipes');
+  if (storage) {
+    return JSON.parse(storage); 
+  }
+
   /**************************/
   // The rest of this method will be concerned with requesting the recipes
   // from the network
@@ -77,6 +83,25 @@ async function getRecipes() {
   //            function (we call these callback functions). That function will
   //            take two parameters - resolve, and reject. These are functions
   //            you can call to either resolve the Promise or Reject it.
+
+  const newRecipes = [];
+  return new Promise(async (resolve, reject) => {
+    for (const url of RECIPE_URLS) {
+      try {
+        const fetched = await fetch(url);
+        const recipe = await fetched.json();
+        newRecipes.push(recipe);
+        if (newRecipes.length === RECIPE_URLS.length) {
+          saveRecipesToStorage(newRecipes);
+          resolve(newRecipes);
+        }
+      } 
+      catch (err) {
+        console.error('Error')
+        return reject(err)
+      }
+    }
+  });
   /**************************/
   // A4-A11 will all be *inside* the callback function we passed to the Promise
   // we're returning
